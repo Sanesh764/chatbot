6c80e60a9e345bad110940a3d39564ddb00931c3
Sanesh <sanesh7644@gmail.com>
2026-07-02 11:59:30 +0530

    fixed error which not conncted database

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
+const dns=require("dns");
+dns.setServers([
+  '1.1.1.1',
+  '8.8.8.8'
+]);
 const connectDB = async () => {
   const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/chatbot';
   
   try {
-    const conn = await mongoose.connect(dbUri, {
-      serverSelectionTimeoutMS: 30000, // Timeout after 5 seconds instead of 30 seconds
-    });
+    const conn = await mongoose.connect(dbUri); 
+      //serverSelectionTimeoutMS: 30000, // Timeout after 5 seconds instead of 30 seconds
+    //});
     console.log(`📡 MongoDB Connected: ${conn.connection.host}`);
     return conn;
   } catch (error) {
